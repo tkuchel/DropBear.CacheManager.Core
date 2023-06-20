91d78875411626ba233a4ab02bf0727e3937f68d
DropBear <tkuchel@users.noreply.github.com>
2023-06-20 16:05:31 +1000

    Fixed missing interface definitions. Version bump to 0.0.9

--- DropBear.CacheManager.Core/CacheManager/ICacheManager.cs
+++ DropBear.CacheManager.Core/CacheManager/ICacheManager.cs
@@ -20,6 +20,20 @@
         Task<bool> ExistsInFasterKvCache(string key);
 
         /// <summary>
+        /// Checks if key exists in disk cache.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public Task<bool> ExistsInDiskCache(string key);
+
+        /// <summary>
+        /// Checks if key exists in SQLite cache.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public Task<bool> ExistsInSQLiteCache(string key);
+
+        /// <summary>
         /// Adds a value to the memory cache.
         /// </summary>
         /// <typeparam name="T">The type of the value.</typeparam>
@@ -40,6 +54,26 @@
         Task<bool> AddToFasterKvCache<T>(string key, T value, TimeSpan expiration);
 
         /// <summary>
+        /// Adds a value to the disk cache.
+        /// </summary>
+        /// <typeparam name="T">The type of the value.</typeparam>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="expiration">The expiration time span.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public Task<bool> AddToDiskCache<T>(string key, T value, TimeSpan expiration);
+
+        /// <summary>
+        /// Adds a value to the SQLite cache.
+        /// </summary>
+        /// <typeparam name="T">The type of the value.</typeparam>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="expiration">The expiration time span.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public Task<bool> AddToSQLiteCache<T>(string key, T value, TimeSpan expiration);
+
+        /// <summary>
         /// Retrieves a value from the memory cache.
         /// </summary>
         /// <typeparam name="T">The type of the value.</typeparam>
@@ -56,6 +90,22 @@
         Task<T?> GetFromFasterKvCache<T>(string key);
 
         /// <summary>
+        /// Retrieves a value from the disk cache.
+        /// </summary>
+        /// <typeparam name="T">The type of the value.</typeparam>
+        /// <param name="key">The key.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public Task<T?> GetFromDiskCache<T>(string key);
+
+        /// <summary>
+        /// Retrieves a value from the SQLite cache.
+        /// </summary>
+        /// <typeparam name="T">The type of the value.</typeparam>
+        /// <param name="key">The key.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public Task<T?> GetFromSQLiteCache<T>(string key);
+
+        /// <summary>
         /// Removes a value from the memory cache.
         /// </summary>
         /// <param name="key">The key.</param>
@@ -68,5 +118,19 @@
         /// <param name="key">The key.</param>
         /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         Task<bool> RemoveFromFasterKvCache(string key);
+
+        /// <summary>
+        /// Removes a value from the disk cache.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public Task<bool> RemoveFromDiskCache(string key);
+
+        /// <summary>
+        /// Removes a value from the SQLite cache.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public Task<bool> RemoveFromSQLiteCache(string key);
     }
 }
